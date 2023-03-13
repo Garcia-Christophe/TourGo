7fba52502167d6c0e9f610169f366f5636bdf62a
G.Christophe <garciachristophe29@gmail.com>
2023-03-13 14:31:19 +0100

    feat(front): design page Sorties

--- front/src/components/CarteSortie/CarteSortie.jsx
+++ front/src/components/CarteSortie/CarteSortie.jsx
@@ -4,21 +4,28 @@ import { BsArrowRightShort } from "react-icons/bs";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-const CarteSortie = ({ sortie, fade }) => {
+const CarteSortie = ({ sortie, fade = "fade-up", fadeDuration = "1000" }) => {
   useEffect(() => {
     // Effets d'affichage
     Aos.init({ duration: 2000 });
   }, []);
 
   return (
-    <div data-aos={fade} data-aos-duration="1000" className="singleDestination">
+    <div
+      data-aos={fade}
+      data-aos-duration={fadeDuration}
+      className="singleDestination"
+    >
       <div className="destImage">
         <img src={sortie.image} alt={sortie.titre} />
         <div className="overlayInfo">
           <h3>Le {sortie.date}</h3>
           <p>{sortie.lieu}</p>
 
-          <BsArrowRightShort className="icon" />
+          <BsArrowRightShort
+            className="icon"
+            onClick={() => (window.location.href = "/")}
+          />
         </div>
       </div>
       <div className="destFooter">
---
+++ front/src/pages/Sorties/Sorties.jsx
@@ -0,0 +1,433 @@
+import React, { useEffect, useState } from "react";
+import "./sorties.css";
+import Navbar from "../../components/Navbar/Navbar";
+import CarteSortie from "../../components/CarteSortie/CarteSortie";
+import Aos from "aos";
+import BG from "../../assets/fondEcranAccueil.jpg";
+import "aos/dist/aos.css";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+
+const Sorties = () => {
+  useEffect(() => {
+    // Personnalisation de la barre de progression
+    for (let e of document.querySelectorAll(
+      'input[type="range"].slider-progress'
+    )) {
+      e.style.setProperty("--value", e.value);
+      e.style.setProperty("--min", e.min === "" ? "0" : e.min);
+      e.style.setProperty("--max", e.max === "" ? "100" : e.max);
+      e.addEventListener("input", () =>
+        e.style.setProperty("--value", e.value)
+      );
+    }
+
+    // Effets d'affichage
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  const sorties = [
+    {
+      nomSortie: "La transléonardetransléonardetransléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "La transléonarde",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "TEST test TEST",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "TEST test TEST",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "TEST test TEST",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "TEST test TEST",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+    {
+      nomSortie: "TEST test TEST",
+      descriptionSortie: "description plus ou moins longue de la sortie",
+      prixSortie: "A côté du terrain de Pontivy.",
+      nbPlaces: 600,
+      nbInscrits: 150,
+      date: "05/06/2023",
+      heure: "08h00",
+      duree: "02h00",
+      lieu: "52 Rue Albirt Loutte, 92900 Brets.",
+      image: BG,
+    },
+  ];
+  const nbSortiesAffichees = 9; // nombre de sorties affichées par page
+  const [indexPremiereSortie, setIndexPremiereSortie] = useState(0); // index de la première sortie affichée
+
+  const [dateDebut, setDateDebut] = useState(""); // date à partir de laquelle on veut voir les sorties
+  const [dateFin, setDateFin] = useState(""); // date jusqu'à laquelle on veut voir les sorties
+  const [prixMax, setPrixMax] = useState(1000); // 0 - 1000 €
+  const [dureeMax, setDureeMax] = useState(72); // 72h = 3 jours
+  const [disponibilite, setDisponibilite] = useState("Toutes"); // Toutes, Places restantes, Complètes
+  const [periode, setPeriode] = useState("Toutes"); // Toutes, Passées, A venir
+
+  return (
+    <>
+      <Navbar navbarTransparente={false} />
+      <section className="page-sorties flex">
+        {/* Filtres */}
+        <section className="sortiesFiltresContainer">
+          <div className="secContainer container">
+            {/* Recherche des sorties par filtres */}
+            <div className="filtresPart grid">
+              {/* Filtres : date de début, date de fin. Type : date picker */}
+              <div className="filtresDates">
+                <div data-aos="fade-up" className="dateDebutDiv">
+                  <label htmlFor="dateDebut">Date de début :</label>
+                  <div className="input flex">
+                    <input
+                      type="date"
+                      value={dateDebut}
+                      onChange={(e) => setDateDebut(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div data-aos="fade-up" className="dateFinDiv">
+                  <label htmlFor="dateFin">Date de fin :</label>
+                  <div className="input flex">
+                    <input
+                      type="date"
+                      value={dateFin}
+                      onChange={(e) => setDateFin(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* Filtres : prix max, durée max. Type : progress bar */}
+              <div className="filtresPrixDuree">
+                <div className="filtresPrix">
+                  <div data-aos="fade-up" className="label_total flex">
+                    <label htmlFor="prix">Prix max :</label>
+                    <h3 className="total">{prixMax} €</h3>
+                  </div>
+                  <div data-aos="fade-up" className="input flex">
+                    <input
+                      type="range"
+                      min="0"
+                      max="1000"
+                      value={prixMax}
+                      step="10"
+                      className="styled-slider slider-progress"
+                      onChange={(e) => setPrixMax(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="filtresDuree">
+                  <div data-aos="fade-up" className="label_duree flex">
+                    <label htmlFor="prix">Durée max :</label>
+                    <h3 className="duree">{dureeMax} h</h3>
+                  </div>
+                  <div data-aos="fade-up" className="input flex">
+                    <input
+                      type="range"
+                      min="0"
+                      max="72"
+                      value={dureeMax}
+                      className="styled-slider slider-progress"
+                      onChange={(e) => setDureeMax(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* Filtres : disponibilités, période. Type : radio buttons */}
+              <div className="filtresDispoPeriode">
+                <div data-aos="fade-up" className="filtresDispo">
+                  <label htmlFor="disponibilite">Disponibilités :</label>
+                  <div className="radioBtns flex">
+                    <div className="dispo-toutes">
+                      <label>
+                        <input
+                          type="radio"
+                          value="Toutes"
+                          checked={disponibilite === "Toutes"}
+                          onChange={(e) => setDisponibilite(e.target.value)}
+                        />
+                        &nbsp;Toutes
+                      </label>
+                    </div>
+                    <div div className="dispo-places-restantes">
+                      <label>
+                        <input
+                          type="radio"
+                          value="Places restantes"
+                          checked={disponibilite === "Places restantes"}
+                          onChange={(e) => setDisponibilite(e.target.value)}
+                        />
+                        &nbsp;Places restantes
+                      </label>
+                    </div>
+                    <div className="dispo-completes">
+                      <label>
+                        <input
+                          type="radio"
+                          value="Completes"
+                          checked={disponibilite === "Completes"}
+                          onChange={(e) => setDisponibilite(e.target.value)}
+                        />
+                        &nbsp;Complètes
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div data-aos="fade-up" className="filtresPeriode">
+                  <label htmlFor="periode">Période :</label>
+                  <div className="radioBtns flex">
+                    <div className="periode-toutes">
+                      <label>
+                        <input
+                          type="radio"
+                          value="Toutes"
+                          checked={periode === "Toutes"}
+                          onChange={(e) => setPeriode(e.target.value)}
+                        />
+                        &nbsp;Toutes
+                      </label>
+                    </div>
+                    <div div className="periode-passees">
+                      <label>
+                        <input
+                          type="radio"
+                          value="Passées"
+                          checked={periode === "Passées"}
+                          onChange={(e) => setPeriode(e.target.value)}
+                        />
+                        &nbsp;Passées
+                      </label>
+                    </div>
+                    <div className="periode-a-venir">
+                      <label>
+                        <input
+                          type="radio"
+                          value="A venir"
+                          checked={periode === "A venir"}
+                          onChange={(e) => setPeriode(e.target.value)}
+                        />
+                        &nbsp;A venir
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="btnDiv flex">
+              <button
+                data-aos="fade-up"
+                data-aos-duration="3000"
+                className="btn"
+              >
+                Rechercher
+              </button>
+            </div>
+          </div>
+        </section>
+
+        {/* Liste des cartes Sorties */}
+        <section className="sortiesSortiesContainer">
+          <div className="secContainer">
+            {/* Titre, sous-titre et flèches */}
+            <div className="secHeader flex">
+              <div className="textDiv">
+                <h2 className="secTitle">Toutes les sorties</h2>
+                <p>Trouvez la sortie qui vous correspond !</p>
+              </div>
+              <div className="iconsDiv flex">
+                <BsArrowLeftShort
+                  className="icon leftIcon"
+                  onClick={() => {
+                    if (indexPremiereSortie > 0) {
+                      setIndexPremiereSortie(
+                        indexPremiereSortie - nbSortiesAffichees
+                      );
+                    }
+                  }}
+                />
+                <BsArrowRightShort
+                  className="icon"
+                  onClick={() => {
+                    if (
+                      sorties.length >
+                      indexPremiereSortie + nbSortiesAffichees
+                    ) {
+                      setIndexPremiereSortie(
+                        indexPremiereSortie + nbSortiesAffichees
+                      );
+                    }
+                  }}
+                />
+              </div>
+            </div>
+            {/* 9 cartes sorties (maximum) correspondantes */}
+            <div className="cartesSorties grid">
+              {sorties.map((sortie, index) => {
+                if (
+                  index >= indexPremiereSortie &&
+                  index < indexPremiereSortie + nbSortiesAffichees
+                )
+                  return (
+                    <CarteSortie
+                      sortie={{
+                        titre: sortie.nomSortie,
+                        infoSupp: sortie.descriptionSortie,
+                        date: sortie.date,
+                        lieu: sortie.lieu,
+                        image: sortie.image,
+                      }}
+                      fade="fade-up"
+                      fadeDuration={1000}
+                    />
+                  );
+                else return null;
+              })}
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Sorties;
